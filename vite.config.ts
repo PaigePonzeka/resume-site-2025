3b8cf3b2546a71518feecd4282bb56d3b6ccd3e5
Paige Ponzeka <pponzeka@yext.com>
2025-04-02 17:57:26 -0700

    Configuring github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/resume-site-2025/',
   plugins: [
     react(),
     svgr(),
